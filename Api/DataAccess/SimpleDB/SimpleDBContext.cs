fa29d077670930278949fd1430981d77420c8b79
Vinge Nikolay <vn2756@gmail.com>
2022-01-31 19:42:29 +0400

    SimpleDB update. Index for DiagramThumbnail.

--- Api/DataAccess/SimpleDB/SimpleDBContext.cs
+++ Api/DataAccess/SimpleDB/SimpleDBContext.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using LiveDiagram.Api.Common;
 using LiveDiagram.Api.Contracts.Common;
 using LiveDiagram.Api.Model;
@@ -24,48 +23,48 @@ namespace LiveDiagram.Api.DataAccess.SimpleDB
 
             builder.Map<Diagram>()
                 .PrimaryKey(x => x.Id)
-                .Field(0, x => x.Title)
-                .Field(1, x => x.Items, new FieldSettings { Compressed = true })
-                .Field(2, x => x.Relations, new FieldSettings { Compressed = true })
-                .Field(3, x => x.Width)
-                .Field(4, x => x.Height)
+                .Field(1, x => x.Title)
+                .Field(2, x => x.Items, new FieldSettings { Compressed = true })
+                .Field(3, x => x.Relations, new FieldSettings { Compressed = true })
+                .Field(4, x => x.Width)
+                .Field(5, x => x.Height)
                 .MakeFunction(() => new Diagram())
                 .PrimaryKeySetFunction((primaryKeyValue, entity) => entity.Id = (string)primaryKeyValue)
                 .FieldSetFunction((fieldNumber, fieldValue, entity) =>
                 {
-                    if (fieldNumber == 0) entity.Title = (string)fieldValue;
-                    if (fieldNumber == 1) entity.Items = (List<DiagramItem>)fieldValue;
-                    if (fieldNumber == 2) entity.Relations = (List<Relation>)fieldValue;
-                    if (fieldNumber == 3) entity.Width = (float)fieldValue;
-                    if (fieldNumber == 4) entity.Height = (float)fieldValue;
+                    if (fieldNumber == 1) entity.Title = (string)fieldValue;
+                    if (fieldNumber == 2) entity.Items = (List<DiagramItem>)fieldValue;
+                    if (fieldNumber == 3) entity.Relations = (List<Relation>)fieldValue;
+                    if (fieldNumber == 4) entity.Width = (float)fieldValue;
+                    if (fieldNumber == 5) entity.Height = (float)fieldValue;
                 });
 
             builder.Map<DiagramThumbnail>()
                 .PrimaryKey(x => x.DiagramId)
-                .Field(0, x => x.Content, new FieldSettings { Compressed = true })
+                .Field(1, x => x.Content, new FieldSettings { Compressed = true })
                 .MakeFunction(() => new DiagramThumbnail())
                 .PrimaryKeySetFunction((primaryKeyValue, entity) => entity.DiagramId = (string)primaryKeyValue)
                 .FieldSetFunction((fieldNumber, fieldValue, entity) =>
                 {
-                    if (fieldNumber == 0) entity.Content = (string)fieldValue;
+                    if (fieldNumber == 1) entity.Content = (string)fieldValue;
                 });
 
             builder.Map<DiagramMeta>()
                 .PrimaryKey(x => x.DiagramId)
-                .Field(0, x => x.Title)
-                .Field(1, x => x.TitleLetter)
-                .Field(2, x => x.TitleNumber)
-                .Field(3, x => x.CreateDate)
-                .Field(4, x => x.UpdateDate)
+                .Field(1, x => x.Title)
+                .Field(2, x => x.TitleLetter)
+                .Field(3, x => x.TitleNumber)
+                .Field(4, x => x.CreateDate)
+                .Field(5, x => x.UpdateDate)
                 .MakeFunction(() => new DiagramMeta())
                 .PrimaryKeySetFunction((primaryKeyValue, entity) => entity.DiagramId = (string)primaryKeyValue)
                 .FieldSetFunction((fieldNumber, fieldValue, entity) =>
                 {
-                    if (fieldNumber == 0) entity.Title = (string)fieldValue;
-                    if (fieldNumber == 1) entity.TitleLetter = (string)fieldValue;
-                    if (fieldNumber == 2) entity.TitleNumber = (int)fieldValue;
-                    if (fieldNumber == 3) entity.CreateDate = (DateTime)fieldValue;
-                    if (fieldNumber == 4) entity.UpdateDate = (DateTime)fieldValue;
+                    if (fieldNumber == 1) entity.Title = (string)fieldValue;
+                    if (fieldNumber == 2) entity.TitleLetter = (string)fieldValue;
+                    if (fieldNumber == 3) entity.TitleNumber = (int)fieldValue;
+                    if (fieldNumber == 4) entity.CreateDate = (DateTime)fieldValue;
+                    if (fieldNumber == 5) entity.UpdateDate = (DateTime)fieldValue;
                 });
 
             builder.Index<DiagramMeta>()
@@ -87,6 +86,11 @@ namespace LiveDiagram.Api.DataAccess.SimpleDB
             builder.Index<DiagramMeta>()
                 .Name("updateDate")
                 .For(x => x.UpdateDate);
+
+            builder.Index<DiagramThumbnail>()
+                .Name("content")
+                .For(x => x.DiagramId)
+                .Include(x => x.Content);
 
             _engine = builder.BuildEngine();
 
